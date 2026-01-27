c73ab69a93898b3876e184be84377559e28bc8f8
Faro19748 <lovernashiga@gmail.com>
2026-01-27 14:33:37 +0700

    up

--- lab/app/build.gradle.kts
+++ lab/app/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.lab"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lab"
