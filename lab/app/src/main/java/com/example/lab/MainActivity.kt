6ce1becb1e1a883d1281b3e0e29e59e71b0856a0
Faro19748 <lovernashiga@gmail.com>
2026-01-27 15:32:48 +0700

    up

--- lab/app/src/main/java/com/example/lab/MainActivity.kt
+++ lab/app/src/main/java/com/example/lab/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             LabTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android1",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- lab/app/src/main/java/com/example/lab/MainActivity2.kt
+++ lab/app/src/main/java/com/example/lab/MainActivity2.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab.ui.theme.LabTheme
@@ -18,24 +21,33 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LabTheme {
+      //      LabTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting2(
-                        name = "Android",
+                        name = "Android2",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
-            }
+    //        }
         }
     }
 }
 
 @Composable
 fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val InputText by remember { mutableStateOf( "") }
+    Column{
+        Text(
+            text = "Hello $name! Say = . . . . ",
+            modifier = modifier
+        )
+        TextField(
+            value = "InputText" ,
+            onValueChange = {it ->
+                InputText = it
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)
