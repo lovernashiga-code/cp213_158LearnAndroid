389b0d1a90b2014378cf00730c3c10484616e350
lovernashiga-code <lovernashiga@gmail.com>
2026-02-03 15:12:25 +0700

    up

--- lab/app/src/main/java/com/example/lab/MainActivity.kt
+++ lab/app/src/main/java/com/example/lab/MainActivity.kt
@@ -6,23 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.lab.ui.theme.LabTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -55,11 +61,63 @@ class MainActivity : ComponentActivity() {
                 Image(
                     painter = painterResource(id = R.drawable.ic_profile),
                     contentDescription = "Profile",
-                    modifier = Modifier.size(350.dp)
+                    modifier = Modifier
+                        .size(350.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 16.dp)
                 )
+                var str by remember { mutableStateOf(10) }
+                var agi by remember { mutableStateOf(15) }
+                var int by remember { mutableStateOf(20) }
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Row {
+                        Button(onClick = {
+                            str = str + 1
+                        }) {
+                            Text(text = "+", fontSize = 32.sp)
+                        }
 
-                //status
+                        Column {
+                            Text(text = "+", fontSize = 32.sp)
+                            Text(text = "str", fontSize = 32.sp)
+                            Text(text = str.toString(), fontSize = 32.sp)
+                            Text(
+                                text = "-",
+                                fontSize = 32.sp,
+                                modifier = Modifier.clickable {
+                                    str = str - 1
+                                }
+                            )
+                        }
+                        Button(onClick = {
+                            agi = agi + 1
+                        }) {
+                            Text(text = "+", fontSize = 32.sp)
+                        }
+                        Column {
+                            Text(text = "+", fontSize = 32.sp)
+                            Text(text = "agi", fontSize = 32.sp)
+                            Text(text = agi.toString(), fontSize = 32.sp)
+                            Text(text = "-", fontSize = 32.sp)
+                        }
+                        Button(onClick = {
+                            int = int + 1
+                        }) {
+                            Text(text = "+", fontSize = 32.sp)
+                        }
+                        Column {
+                            Text(text = "+", fontSize = 32.sp)
+                            Text(text = "int", fontSize = 32.sp)
+                            Text(text = int.toString(), fontSize = 32.sp)
+                            Text(text = "-", fontSize = 32.sp)
+                        }
+                        
+                }
             }
         }
     }
+}
 }
